85d298ba19f80894ebfd5f7840e5623030f59510
KikoChico <88805604+KikoChico@users.noreply.github.com>
2022-06-30 17:32:58 -0400

    Add files via upload

---
+++ yuh yuh #1.py
@@ -0,0 +1,52 @@
+from tkinter import *
+from PIL import ImageTk, Image
+root=Tk()
+
+root.title("endless dice game")
+root.geometry("600x400")
+
+#sets background color fpr entire window
+root.configure(background="royal blue")
+
+#how to insert a jpg image
+img=ImageTk.PhotoImage(Image.open ("dice 1.png"))
+
+player1 = Label(root, text = "Player 1", bg = "orange", fg = "black")
+player1.place(relx = 0.1, rely = 0.3, anchor = CENTER)
+
+player2 = Label(root, text = "Player 2", bg = "orange", fg = "black")
+player2.place(relx = 0.9, rely = 0.3, anchor = CENTER)
+
+player_1_score_label = Label(root , bg = "orange", fg = "black")
+player_1_score_label.place(relx = 0.1, rely = 0.4 , anchor = CENTER)
+
+player_2_score_label = Label(root , bg = "orange", fg = "black")
+player_2_score_label.place(relx = 0.9, rely = 0.4 , anchor = CENTER)
+
+random_dice_label = Label(root,bg = "chocolate1", fg = "white")
+random_dice_label.place(relx = 0.5, rely = 0.5, anchor = CENTER)
+
+player1_score = 0
+def player1():
+    global player1_score
+    random_no = random.randint(1,6)
+    random_dice_label["text"] = "Player 1 dice random number : " + str(random_no)
+    player1_score = player1_score + random_no
+    player1score_label["text"] = str(player1_score)
+    
+player1_btn = Button(root, image = img, command = player1)
+player1_btn.place(relx = 0.1, rely = 0.6, anchor = CENTER)
+
+player2_score = 0
+def player2():
+    global player2_score
+    random_no2 = random.randint(1,6)
+    random_dice_label["text"] = "Player 2 dice random number : " + str(random_no2)
+    player2_score = player2_score + random_no2
+    player2score_label["text"] = str(player2_score)
+    
+player_2_btn = Button(root, image = img, command = player2)
+player2_btn.place(relx = 0.9, rely = 0.6, anchor = CENTER)
+
+
+root.mainloop()
